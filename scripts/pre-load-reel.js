912a4242dee161e09ab3e74216ef3c0ab5bbcd23
boshi <boshimoto@protonmail.com>
2022-03-09 21:57:32 -0500

    javascript: working on logic to have image reel in projects

---
+++ scripts/pre-load-reel.js
@@ -0,0 +1,11 @@
+// new image array
+let reelImages = new Array()
+
+reelImages[0] = new Image() // create new instance of image object
+reelImages[0].src = "..\images\projects\codebase-showdown.PNG" // set image src property to image path, preloading image in the process
+reelImages[1] = new Image()
+reelImages[1].src = "..\images\projects\ps-download-showdown.PNG"
+reelImages[2] = new Image()
+reelImages[2].src = "..\images\projects\running-showdown.PNG"
+reelImages[3] = new Image()
+reelImages[3].src = "..\images\projects\results-showdown.PNG"
