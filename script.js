ed175d2b6dcf8c0c370fce542e3b4e2e8943b61f
Boshi <69194793+boshimoto@users.noreply.github.com>
2022-08-11 06:11:15 -0400

    condensed js function for animation, styling updates to homepage

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ const navBarDrop = (previousYPos) => {
 
 const debouncedNavBarDrop = debounce(navBarDrop, 300);
 
-const allCards = document.querySelectorAll('.cta__card');
-const cardsFrontFace = document.querySelectorAll('.cta__face-front');
+const allCards = document.querySelectorAll('.connect__card');
+const cardsFrontFace = document.querySelectorAll('.connect__face-front');
 
 allCards.forEach((element) => {
     element.addEventListener('mouseover', () => {
@@ -63,11 +63,9 @@ allCards.forEach((element) => {
     })
 });
 
-
 const ctaArticleIndex = document.querySelectorAll('.content-index');
 const ctaArticleTitle = document.querySelectorAll('.content-title');
 const ctaArticleBody = document.querySelectorAll('.content-body');
-
 
 const animateElement = (element, attribute='', pageYPosition, elementTopPosition) => {
     // if page Y coordinate exceeds an element top Y coordinate
@@ -76,77 +74,16 @@ const animateElement = (element, attribute='', pageYPosition, elementTopPosition
     } else element.classList.remove(attribute);
 };
 
-const animatedArticleIndex0 = () => {
-    let pageYPosition = (previousYPos - 1300) + ctaArticleIndex[0].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleIndex[0].getBoundingClientRect().top;
-    return animateElement(ctaArticleIndex[0], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleTitle0 = () => {
-    let pageYPosition = (previousYPos - 1500) + ctaArticleTitle[0].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleTitle[0].getBoundingClientRect().top;
-    return animateElement(ctaArticleTitle[0], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleBody0 = () => {
-    let pageYPosition = (previousYPos - 1500) + ctaArticleBody[0].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleBody[0].getBoundingClientRect().top;
-    return animateElement(ctaArticleBody[0], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleIndex1 = () => {
-    let pageYPosition = (previousYPos - 1800) + ctaArticleIndex[1].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleIndex[1].getBoundingClientRect().top;
-    return animateElement(ctaArticleIndex[1], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleTitle1 = () => {
-    let pageYPosition = (previousYPos - 2100) + ctaArticleTitle[1].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleTitle[1].getBoundingClientRect().top;
-    return animateElement(ctaArticleTitle[1], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleBody1 = () => {
-    let pageYPosition = (previousYPos - 2100) + ctaArticleBody[1].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleBody[1].getBoundingClientRect().top;
-    return animateElement(ctaArticleBody[1], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleIndex2 = () => {
-    let pageYPosition = (previousYPos - 2400) + ctaArticleIndex[2].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleIndex[2].getBoundingClientRect().top;
-    return animateElement(ctaArticleIndex[2], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleTitle2 = () => {
-    let pageYPosition = (previousYPos - 2700) + ctaArticleTitle[2].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleTitle[2].getBoundingClientRect().top;
-    return animateElement(ctaArticleTitle[2], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleBody2 = () => {
-    let pageYPosition = (previousYPos - 2700) + ctaArticleBody[2].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleBody[2].getBoundingClientRect().top;
-    return animateElement(ctaArticleBody[2], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleIndex3 = () => {
-    let pageYPosition = (previousYPos - 2900) + ctaArticleIndex[3].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleIndex[3].getBoundingClientRect().top;
-    return animateElement(ctaArticleIndex[3], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleTitle3 = () => {
-    let pageYPosition = (previousYPos - 3200) + ctaArticleTitle[3].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleTitle[3].getBoundingClientRect().top;
-    return animateElement(ctaArticleTitle[3], 'slide-effect', pageYPosition, elementTopPosition)
-};
-
-const animatedArticleBody3 = () => {
-    let pageYPosition = (previousYPos - 3200) + ctaArticleBody[3].getBoundingClientRect().top;
-    let elementTopPosition = ctaArticleBody[3].getBoundingClientRect().top;
-    return animateElement(ctaArticleBody[3], 'slide-effect', pageYPosition, elementTopPosition)
-};
+// set offSet for the first elements page position to trigger animation event and increment it based on position of other elements
+const animateElementCoordinates = (previousYPos, offSet, offSetIncrement, newClass, elementArray) => {
+    let count = offSet
+    elementArray.forEach((element) => {
+        let elementTopPosition = element.getBoundingClientRect().top;
+        let pageYPosition = (previousYPos - count) + elementTopPosition;
+        count += offSetIncrement;
+        return animateElement(element, newClass, pageYPosition, elementTopPosition)
+    })
+}
 
 // const spinCards = () => {
 //     const forEachCard = (card) => {
@@ -170,22 +107,9 @@ const animatedArticleBody3 = () => {
 const documentScroll = () => {
     debouncedNavBarDrop(previousYPos);
 
-    animatedArticleIndex0(previousYPos);
-    animatedArticleTitle0(previousYPos);
-    animatedArticleBody0(previousYPos);
-
-    animatedArticleIndex1(previousYPos);
-    animatedArticleTitle1(previousYPos);
-    animatedArticleBody1(previousYPos);
-
-    animatedArticleIndex2(previousYPos);
-    animatedArticleTitle2(previousYPos);
-    animatedArticleBody2(previousYPos);
-
-    animatedArticleIndex3(previousYPos);
-    animatedArticleTitle3(previousYPos);
-    animatedArticleBody3(previousYPos);
-
+    animateElementCoordinates(previousYPos, 1500, 900, 'slide-effect', ctaArticleIndex);
+    animateElementCoordinates(previousYPos, 1650, 900, 'slide-effect', ctaArticleTitle);
+    animateElementCoordinates(previousYPos, 1650, 900, 'slide-effect', ctaArticleBody);
 
     // spinCards();
     previousYPos = window.pageYOffset;
@@ -199,7 +123,7 @@ document.addEventListener('scroll', documentScroll);
 // NEW IN PROGRESS
 
 const blockBtnContainer = document.getElementById('code__block-btn');
-const blockBtn = document.querySelector('.block-btn');
+const blockBtn = document.querySelector('.code-btn');
 const codeBlock = document.querySelector('.code__block');
 const codeImage = document.getElementById('code');
 const codeContainer = document.querySelector('.code__container');
@@ -216,16 +140,15 @@ const loop = async () => {
 
     for (let square of squares) {
         square.classList.add('bye');
-        await wait(20)
+        await wait(10)
     }
-}
+};
 
 const updateBox = () => {
     loop().then(addImage).catch( (error) => { console.error(error); });
 };
 
-
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 184; i++) {
     const newBlock = document.createElement('div');
     newBlock.classList.add('block');
     codeBlock.appendChild(newBlock);
@@ -233,12 +156,12 @@ for (let i = 0; i < 100; i++) {
 
 const updateBtn = () => {
     blockBtn.classList.add('bye');
-}
+};
 
 const addImage = () => {
     codeContainer.style.overflowY = "scroll";
     codeImage.src = '/pexels-pixabay-358238.jpg';
-}
+};
 
 blockBtnContainer.addEventListener('click', updateBox);
 blockBtn.addEventListener('click', updateBtn);
