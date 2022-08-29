25b4df9e213153b544df12a538cb18367d172197
Boshi <69194793+boshimoto@users.noreply.github.com>
2022-08-29 01:04:36 -0400

    refactoring js imports/exports

--- scripts/script.js
+++ scripts/script.js
@@ -1,86 +1,45 @@
-/* Style Guide
-https://airbnb.io/javascript/ */
+import { debounce, throttle, wait } from './performance.js';
+import { iconSwap, navBarDrop } from './navBar.js';
+
+/* Styling https://airbnb.io/javascript/ */
 
 const navBar = document.getElementById('nav-banner');
 const navBtn = document.querySelector('.nav__menu-btn');
 const menu = document.querySelector('.nav__menu');
 const menuItems = document.querySelector('.nav__menu-items');
-const body = document.body
+const body = document.body;
 let previousYPos;
 
-const iconSwap = (element, starturl='', endurl='') => {
-    if (element.src.endsWith(starturl)) {
-        element.src = endurl;
-        menu.classList.add('expand');
-        menuItems.classList.add('visible');
-        body.classList.add('no-scroll');
-    } else if (element.src.endsWith(endurl)) {
-        element.src = starturl;
-        menu.classList.remove('expand');
-        menuItems.classList.remove('visible');
-        body.classList.remove('no-scroll');
-    }
-};
+const navMenuExpand = () => {
+    return (
+        menu.classList.add('expand'),
+        menuItems.classList.add('visible'),
+        body.classList.add('no-scroll')
+    )
+}
+
+const navMenuShrink = () => {
+    return (
+        menu.classList.remove('expand'),
+        menuItems.classList.remove('visible'),
+        body.classList.remove('no-scroll')
+    )
+}
 
 const navIcon = () => {
-    return iconSwap(navBtn.firstChild.nextElementSibling,
+    const navElement = navBtn.firstChild.nextElementSibling;
+    return iconSwap(
+        navElement,
         '/img/nav/menu-ready.svg',
-        '/img/nav/menu-close.svg')
+        '/img/nav/menu-close.svg',
+        navMenuExpand,
+        navMenuShrink
+    )
 };
 
 navBtn.addEventListener('click', navIcon);
 
-/* https://www.freecodecamp.org/news/javascript-debounce-example/
-https://levelup.gitconnected.com/debounce-in-javascript-improve-your-applications-performance-5b01855e086 */
-const debounce = (func, delay = 300) => {
-    let timeout;
-
-    const executeTimeout = (...args) => {
-        clearTimeout(timeout);
-        const setTime = () => {
-            func.apply(this, args);
-        }
-        timeout = setTimeout(setTime, delay);
-    };
-
-    return executeTimeout
-};
-
-// https://codeburst.io/throttling-and-debouncing-in-javascript-b01cad5c8edf
-const throttle = (func, limit) => {
-    let lastFunc
-    let lastRan
-    return function() {
-        const context = this
-        const args = arguments
-        if (!lastRan) {
-            func.apply(context, args)
-            lastRan = Date.now()
-        } else {
-            clearTimeout(lastFunc)
-            lastFunc = setTimeout(function() {
-                if ((Date.now() - lastRan) >= limit) {
-                    func.apply(context, args)
-                    lastRan = Date.now()
-                }
-            }, limit - (Date.now() - lastRan))
-        }
-    }
-}
-
-const navBarDrop = (previousYPos) => {
-    let currentYPos = window.pageYOffset;
-
-    if (currentYPos <= 100) {
-        navBar.classList.remove('active');
-    } else if(currentYPos > previousYPos) {
-        navBar.classList.remove('active');
-    } else if(currentYPos < previousYPos) {
-        navBar.classList.add('active');
-    }
-};
-
-const debouncedNavBarDrop = debounce(navBarDrop);
+const throttledNavBarDrop = throttle(navBarDrop);
 
 const ctaIndexFrames = document.querySelectorAll('.cta__content-index-frame');
 const ctaTitleFrames = document.querySelectorAll('.cta__content-title-frame');
@@ -183,7 +142,7 @@ const throttledCheckCondition = (conditionFn) => {
 
 const documentScroll = () => {
 
-    debouncedNavBarDrop(previousYPos);
+    throttledNavBarDrop(previousYPos);
     
     throttledCheckCondition(checkCondition(1, throttledCtaEvents))
     throttledCheckCondition(checkCondition(3, throttledConnectEvents))
@@ -198,13 +157,6 @@ document.addEventListener('scroll', documentScroll);
 const blockBtnContainer = document.getElementById('engine__block-btn');
 const blockBtn = document.querySelector('.engine-btn');
 const blocks = document.querySelector('.engine__block-container');
-
-
-// https://thewebdev.info/2022/02/09/how-to-create-pause-or-delay-in-a-javascript-for-loop/#:~:text=JavaScript%20for%20loop%3F-,To%20create%20pause%20or%20delay%20in%20a%20JavaScript%20for%20loop,with%20a%20for%2Dof%20loop.&text=to%20define%20the%20wait%20function,to%20loop%20through%20an%20array.
-
-const wait = (ms) => new Promise( (resolve) => {
-    setTimeout(resolve, ms)
-});
 
 const updateBox = async () => {
     const squares = document.querySelectorAll('.block');
